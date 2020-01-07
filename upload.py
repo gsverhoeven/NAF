2c0e89cb42e56b54f34b57e5828caeaa66feb593
ketilkn <ketilkn@gmail.com>
2020-01-07 19:25:14 +0100

    Use argparse.FileType to easily support for shell data streams

--- upload.py
+++ upload.py
@@ -18,33 +18,30 @@ def load_file(filename):
     return file
 
 
-def upload_file(file, url, secret=False):
+def upload_file(csv_file, url, secret=False):
     LOG.debug('Connecting to %s', url)
     header = session.build_header()
     form = {'submit': 'submit'}
     if secret:
         form['top_secret'] = secret
 
-    response = False
-    with open(file.as_posix(), 'r') as csv_file:
-        LOG.info('Uploading file')
-        response = requests.post(url, files={'csv_file': csv_file}, headers=header, data=form)
-        if not response:
-            LOG.error('Problem uploading file %s %s', response.status_code, response.reason)
-            LOG.debug('%s', response.text)
-            return False
-        LOG.info('Upload OK!')
-        return response.text
-
-
-def upload_rank(filename, secret=False, url='https://member.thenaf.net/glicko/import.php'):
-    LOG.debug('Uploading %s to %s', filename, url)
-
-    file = load_file(filename)
-    if not file:
-        LOG.error('Error loading file %s', filename)
+    LOG.info('Uploading file')
+    response = requests.post(url, files={'csv_file': csv_file}, headers=header, data=form)
+    if not response:
+        LOG.error('Problem uploading file %s %s', response.status_code, response.reason)
+        LOG.debug('%s', response.text)
         return False
-    
+    LOG.info('Upload OK!')
+    return response.text
+
+
+def upload_rank(file, secret=False, url='https://member.thenaf.net/glicko/import.php'):
+    LOG.debug('Uploading %s to %s', file.name, url)
+
+    if not file:
+        LOG.error('Error loading file %s', file.name)
+        return False
+
     response = upload_file(file, url, secret)
     if not response:
         return False
@@ -68,14 +65,14 @@ def main():
 
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--debug', action='store_true')
-    arg_parser.add_argument('filename', type=str)
+    arg_parser.add_argument('infile', type=argparse.FileType('r'))
     arg_parser.add_argument('--target-url', default=config['target_url'], action='store_true')
     arg_parser.add_argument('--top-secret', default=config['top_secret'], action='store_true')
 
     arguments = arg_parser.parse_args()
 
     LOG.debug("Using arguments %s", arguments)
-    upload_rank(filename=arguments.filename, url=arguments.target_url, secret=arguments.top_secret)
+    upload_rank(file=arguments.infile, url=arguments.target_url, secret=arguments.top_secret)
 
 
 if __name__=='__main__':
